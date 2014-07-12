e5595cafd6af149b6f1c3bf212eda190bdd635f7
Natalie <blackburn87@gmail.com>
2014-07-12 11:32:29 -0700

    fix model relationships

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Post do
-  belongs_to :user
+  it { should belong_to :user }
   
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 describe User do
-  
+  it { should have_many :posts }
   it { should validate_presence_of :email }
 end
